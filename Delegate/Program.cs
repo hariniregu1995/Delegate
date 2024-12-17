30028da3dc683868dd848a0c74ea6cb9dfd2cc2c
hariniregu1995 <98214414+hariniregu1995@users.noreply.github.com>
2024-12-17 12:29:36 +0530

    New Commit

--- Delegate/Program.cs
+++ Delegate/Program.cs
@@ -15,17 +15,9 @@ namespace EventsInCSharp
     {
         static void Main(string[] args)
         {
-            MyClass myClass1 = new MyClass();
-            myClass1.MyEvent += new MyClass.MyDelegate(myClass1_MyEvent);
-            Console.WriteLine("Please enter a message\n");
-            string msg = Console.ReadLine();
-            myClass1.RaiseEvent(msg);
-            Console.Read();
+            Console.WriteLine("Neww Repository");
         }
-        static void myClass1_MyEvent(string message)
-        {
-            Console.WriteLine("Your Message is: {0}", message);
-        }
+       
     }
 }
 
