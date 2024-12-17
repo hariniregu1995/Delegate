9669157cdb5ff78ec8222244cbdd6ea0ce62320e
hariniregu1995 <98214414+hariniregu1995@users.noreply.github.com>
2024-12-17 12:32:57 +0530

    Update Program.cs

--- Delegate/Program.cs
+++ Delegate/Program.cs
@@ -7,7 +7,7 @@ namespace EventsInCSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("New Repository");
+            Console.WriteLine("New Repository from GIT");
         }
        
     }
