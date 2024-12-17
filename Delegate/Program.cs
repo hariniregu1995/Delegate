3326f00b571e92cca77313e94eb0ea13c79c7dd6
hariniregu1995 <98214414+hariniregu1995@users.noreply.github.com>
2024-12-17 12:31:03 +0530

    new

--- Delegate/Program.cs
+++ Delegate/Program.cs
@@ -2,20 +2,12 @@
 using System.Collections.Generic;
 namespace EventsInCSharp
 {
-    public class MyClass
-    {
-        public delegate void MyDelegate(string message);
-        public event MyDelegate MyEvent;
-        public void RaiseEvent(string message)
-        {
-            if (MyEvent != null) MyEvent(message);
-        }
-    }
+ 
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Neww Repository");
+            Console.WriteLine("New Repository");
         }
        
     }
